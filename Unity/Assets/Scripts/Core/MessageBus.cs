8d840de0dc369a02d9fd9424a9c484c76279750e
RupeWard <rupe@rupertward.co.uk>
2016-05-13 13:31:08 +0100

    Add MessageBus class

---
+++ Unity/Assets/Scripts/Core/MessageBus.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class MessageBus : MonoBehaviour
+{
+	private static MessageBus _instance;
+
+	//	public Action<BOotsaData> bootsChanged;
+
+	public static MessageBus instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = new GameObject("MessageBus").AddComponent<MessageBus>();
+			}			
+			return _instance;
+		}
+	}
+
+	/*
+	public void dispatchBootsChanged(BootsData bd)
+	{
+		if (bootsChanged != null)
+		{
+//			Debug.Log ("Sending boots changed to " + bd.id);
+			bootsChanged (bd);
+		} 
+		else
+		{
+			Debug.LogWarning ("No boots changed action ");
+		}
+	}
+	*/
+
+	public void clear()
+	{
+//		bootsChanged = null;
+	}
+}
