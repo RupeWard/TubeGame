58f9700ae0b638d178b9461ceb34a1bc17a9c29d
RupeWard <rupe@rupertward.co.uk>
2016-05-17 16:55:34 +0100

    Flowzone impulse to player

--- Unity/Assets/Scripts/Player/Player.cs
+++ Unity/Assets/Scripts/Player/Player.cs
@@ -5,9 +5,21 @@ namespace RJWard.Tube.Player
 {
 	public class Player : MonoBehaviour
 	{
+		static readonly bool DEBUG_COLLISIONS = true;
+
 		private Transform cachedTransform_;
 		private Rigidbody body_;
 		public PlayerCam cam;
+
+		private FlowZone_Linear currentFlowZone = null;
+
+		private void FixedUpdate()
+		{
+			if (currentFlowZone != null)
+			{
+				body.AddForce( currentFlowZone.directionVector * Time.deltaTime, ForceMode.Impulse );
+			}
+		}
 
 		public Rigidbody body
 		{
@@ -33,6 +45,54 @@ namespace RJWard.Tube.Player
 			body_.velocity = Vector3.zero;
 
 			cam.gameObject.SetActive( true );
+		}
+
+		private void OnTriggerEnter( Collider other )
+		{
+			if (DEBUG_COLLISIONS)
+			{
+				Debug.Log( "TRIGGER ENTER: " + gameObject.name + " " + other.gameObject.name );
+			}
+			FlowZone_Linear newFz = other.gameObject.GetComponent<FlowZone_Linear>( );
+			if (newFz != null)
+			{
+				if (newFz != currentFlowZone)
+				{
+					if (DEBUG_COLLISIONS)
+					{
+						Debug.Log( "TRIGGER ENTER PLAYER: '" + gameObject.name + "' to new flow zone '" + other.gameObject.name +"' from "+currentFlowZone);
+					}
+					currentFlowZone = newFz;
+				}
+				else
+				{
+					Debug.LogWarning( "TRIGGER ENTER PLAYER: '" + gameObject.name + "' to SAME flow zone '" + other.gameObject.name + "' from " + currentFlowZone.gameObject.name );
+				}
+			}
+		}
+
+		private void OnTriggerExit( Collider other )
+		{
+			if (DEBUG_COLLISIONS)
+			{
+				Debug.Log( "TRIGGER EXIT: " + gameObject.name + " " + other.gameObject.name );
+			}
+		}
+
+		private void OnCollisionEnter( Collision collision )
+		{
+			if (DEBUG_COLLISIONS)
+			{
+				Debug.Log( "COLLISION ENTER: " + gameObject.name + " " + collision.gameObject.name );
+			}
+		}
+
+		private void OnCollisionExit( Collision collision )
+		{
+			if (DEBUG_COLLISIONS)
+			{
+				Debug.Log( "COLLISION EXIT: " + gameObject.name + " " + collision.gameObject.name );
+			}
 		}
 
 	}
--- Unity/Assets/Scripts/Player/Thingy.cs
+++ Unity/Assets/Scripts/Player/Thingy.cs
@@ -5,12 +5,47 @@ namespace RJWard.Tube.Player
 {
 	public class Thingy : MonoBehaviour
 	{
+		static readonly bool DEBUG_COLLISIONS = true;
+
 		public Transform cachedTransform;
 		
 		private void Awake( )
 		{
 			cachedTransform = transform;
 		}
+
+		private void OnTriggerEnter( Collider other )
+		{
+			if (DEBUG_COLLISIONS)
+			{
+				Debug.Log( "TRIGGER ENTER: " + gameObject.name + " " + other.gameObject.name );
+			}
+		}
+
+		private void OnTriggerExit( Collider other )
+		{
+			if (DEBUG_COLLISIONS)
+			{
+				Debug.Log( "TRIGGER EXIT: " + gameObject.name + " " + other.gameObject.name );
+			}
+		}
+
+		private void OnCollisionEnter( Collision collision )
+		{
+			if (DEBUG_COLLISIONS)
+			{
+				Debug.Log( "COLLISION ENTER: " + gameObject.name + " " + collision.gameObject.name );
+			}
+		}
+
+		private void OnCollisionExit( Collision collision )
+		{
+			if (DEBUG_COLLISIONS)
+			{
+				Debug.Log( "COLLISION EXIT: " + gameObject.name + " " + collision.gameObject.name );
+			}
+		}
+
 	}
 }
 
