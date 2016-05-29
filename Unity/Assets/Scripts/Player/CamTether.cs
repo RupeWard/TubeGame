5acf7f482d6708ec022d723fef68c5e7b2d172b4
RupeWard <rupe@rupertward.co.uk>
2016-05-29 16:03:10 +0100

    New tethered camera

---
+++ Unity/Assets/Scripts/Player/CamTether.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RJWard.Tube.Player
+{
+	public class CamTether : MonoBehaviour
+	{
+		public Player player;
+		public float targetDistance = 5f;
+
+		private Transform camTransform_;
+		public UnityEngine.Camera tetheredCamera;
+
+        private Transform cachedTransform_;
+
+		private Vector3 directionVector_ = Vector3.zero;
+
+		private void Awake()
+		{
+			cachedTransform_ = transform;
+			camTransform_ = tetheredCamera.transform;
+
+		}
+
+		private void Start()
+		{
+			RJWard.Tube.UI.UIManager.Instance.SetCameraToViewport( tetheredCamera );
+		}
+
+		void Update( )
+		{
+			if (player != null && player.isActiveAndEnabled)
+			{
+				player.UpdateDirection( ref directionVector_ );
+				cachedTransform_.position = player.cachedTransform.position + directionVector_ * targetDistance;
+
+				if (camTransform_ != null )
+				{
+					camTransform_.LookAt( player.cachedTransform.position );
+				}
+			}
+		}
+	}
+
+}
---
+++ Unity/Assets/Scripts/Tube/TubeManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RJWard.Tube
+{
+	public class TubeManager : RJWard.Core.Singleton.SingletonApplicationLifetimeLazy<TubeManager>
+	{
+
+	}
+}
