37bdde0db50b10d4e7d263c7323af56ef15d31c4
RupeWard <rupe@rupertward.co.uk>
2016-06-03 01:56:42 +0100

    Sql build fix. Need different DLLs for android build

--- Unity/Assets/Scripts/Core/Version.cs
+++ Unity/Assets/Scripts/Core/Version.cs
@@ -7,7 +7,7 @@ namespace RJWard.Core
 	{
 		// Change this to match whenever updating version number in build settings
 		// Put a 'D' at the start to make a Dev version 
-		public static VersionNumber versionNumber = new VersionNumber( "D0.0.3 (15)" );
+		public static VersionNumber versionNumber = new VersionNumber( "D0.0.3 (19)" );
 
 		public static string platformLabel
 		{
