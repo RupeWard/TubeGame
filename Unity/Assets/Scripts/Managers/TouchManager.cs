a12a0bbd226dc74e163aa28c2dc697bcadd10687
RupeWard <rupe@rupertward.co.uk>
2016-06-04 15:44:15 +0100

    TouchManager & control panel stubs

---
+++ Unity/Assets/Scripts/Managers/TouchManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : RJWard.Core.Singleton.SingletonSceneLifetime<TouchManager>
+{
+	public static readonly bool DEBUG_TOUCHES = true;
+
+	private int numTouches_ = 0;
+
+	private void Update()
+	{
+		int newNumTouches = Input.touches.Length;
+        if (newNumTouches != numTouches_)
+		{
+			if (DEBUG_TOUCHES)
+			{
+				Debug.Log( "Num touches changed from " + numTouches_ + " to " + newNumTouches );
+			}
+			numTouches_ = newNumTouches;
+		}
+		else
+		{
+//			Debug.Log( "Still "+numTouches_+" touches");
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/UI/PlayerControlPanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlPanel : MonoBehaviour
+{
+	static private readonly bool DEBUG_CONTROLS = true;
+
+	#region inspector hooks
+
+	public RectTransform centreMarker;
+	public RectTransform centreButton;
+
+	#endregion inspector hooks
+
+}
